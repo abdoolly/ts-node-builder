905812eb073dd90a7c0052f3d2ec343c93291660
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2020-04-29 11:28:14 +0200

    feat(copy): add glob support

--- helpers/copy.ts
+++ helpers/copy.ts
@@ -1,6 +1,4 @@
-import * as shell from 'shelljs';
-import { existsSync } from 'fs';
-
+import * as cpx from 'cpx';
 
 export interface Copy {
     from: string;
@@ -8,8 +6,8 @@ export interface Copy {
 }
 
 /**
- * @description apply the copy array using the copy function 
- * @param arr 
+ * @description apply the copy array using the copy function
+ * @param arr
  */
 export function copyArray(arr: Copy[]) {
     for (let copyOp of arr) {
@@ -19,28 +17,9 @@ export function copyArray(arr: Copy[]) {
 
 /**
  * @description function that copy files or folders and take the from path and the toPath
- * @param fromPath 
- * @param toPath 
+ * @param fromPath
+ * @param toPath
  */
 export function copy(fromPath: string, toPath: string) {
-
-    // validate file paths
-    if (!isValidPath(fromPath))
-        return console.error(`Path "${fromPath}" does not exist`);
-
-    if (!isValidPath(toPath))
-        shell.mkdir('-p', toPath);
-
-    shell.cp('-ru', fromPath, toPath);
-}
-
-/**
- * @description function that validate that file or folder actually exists
- * @param path 
- */
-function isValidPath(path: string, ) {
-    if (!existsSync(path))
-        return false;
-
-    return true;
+    cpx.copySync(fromPath, toPath);
 }
